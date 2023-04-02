cc5c3af2707ac3c33beb0618e99858d966e63c27
Fatih Ali <fatihali@stu.aydin.edu.tr>
2023-04-02 14:46:02 +0300

    first commit

---
+++ Bean/src/main/java/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+
+        Parrot p2 = context.getBean("miki",Parrot.class);
+        System.out.println(p2.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
+
+
+
+    }
+}
