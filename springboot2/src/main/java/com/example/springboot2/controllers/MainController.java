021282c484b17a82f8245f388ac9f288127f7e2f
Fatih Ali <fatihali@stu.aydin.edu.tr>
2023-04-16 19:37:57 +0300

    add springboot3

--- springboot2/src/main/java/com/example/springboot2/controllers/MainController.java
+++ springboot2/src/main/java/com/example/springboot2/controllers/MainController.java
@@ -16,4 +16,5 @@ public class MainController {
     return "home.html";
     }
 
+    
 }
---
+++ springboot3/src/main/java/com/example/springboot3/controllers/ProductsController.java
@@ -0,0 +1,36 @@
+package com.example.springboot3.controllers;
+
+import com.example.springboot3.model.Product;
+import com.example.springboot3.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProductsController {
+
+    private final ProductService productService;
+
+    public ProductsController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String viewProducts(Model model){
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+        return "products.html";
+    }
+
+    @PostMapping("/products")
+    public String addProduct(Product p, Model model){
+
+        productService.addProduct(p);
+
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+}
