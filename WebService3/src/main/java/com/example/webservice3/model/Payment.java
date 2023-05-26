70d8848209cfe640b7ed17d39a98d6fdb661153b
Fatih Ali <fatihali@stu.aydin.edu.tr>
2023-05-27 00:10:45 +0300

    PaymentsController added

--- WebService3/src/main/java/com/example/webservice3/model/Payment.java
+++ WebService3/src/main/java/com/example/webservice3/model/Payment.java
@@ -8,16 +8,14 @@ public class Payment {
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public double getAmount() {
         return amount;
     }
-
     public void setAmount(double amount) {
         this.amount = amount;
     }
+
 }
