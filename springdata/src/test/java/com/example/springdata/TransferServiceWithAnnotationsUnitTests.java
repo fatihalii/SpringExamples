25a7b7fb7e21b7fafc10ab98c55aa235d67676e9
Fatih Ali <fatihali@stu.aydin.edu.tr>
2023-06-05 23:39:08 +0300

    Unit and Integration tests for TransferService added

---
+++ springdata/src/test/java/com/example/springdata/TransferServiceWithAnnotationsUnitTests.java
@@ -0,0 +1,52 @@
+package com.example.springdata;
+
+import com.example.springdata.exceptions.AccountNotFoundException;
+import com.example.springdata.model.Account;
+import com.example.springdata.repositories.AccountRepository;
+import com.example.springdata.services.TransferService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class TransferServiceWithAnnotationsUnitTests {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private TransferService transferService;
+
+    @Test
+    public void moneyTransferDestinationAccountNotFoundFlow(){
+
+        Account sender = new Account();
+        sender.setId(1);
+        sender.setAmount(new BigDecimal(1000));
+
+        given(accountRepository.findById(1L))
+                .willReturn(Optional.of(sender));
+
+        given(accountRepository.findById(2L))
+                .willReturn(Optional.empty());
+
+        assertThrows(AccountNotFoundException.class,
+                () -> transferService.transferMoney(1,2, new BigDecimal(100)));
+
+        verify(accountRepository, never())
+                .changeAmount(anyLong(), any());
+
+    }
+}
